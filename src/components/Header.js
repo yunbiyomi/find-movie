0ae1807ad58ab642ce694c06dfc52df14befb2d7
yunbiyomi <yunbiyomi@naver.com>
2023-09-27 02:19:55 +0900

    💄 Style : Headline 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,6 @@ export default class Header extends Component {
   }
   render() {
     this.el.innerHTML = /* html */ `
-      <a href="#/">메인</a>
-      <a href="#/about">상세페이지</a>
     `
   }
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,12 @@
 import { Component } from "../core";
 import Headline from "../components/Headline";
-import Search from "../components/Search";
 
 export default class Home extends Component {
   render() {
     const headline = new Headline().el
-    const search = new Search().el
 
     this.el.append(
-      headline,
-      search
+      headline
     );
   }
 }
