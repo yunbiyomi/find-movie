474f1e883a05d271c32236c84dee5e7e7ddcd3e4
yunbiyomi <yunbiyomi@naver.com>
2023-10-05 21:59:32 +0900

    🐛 Fix : 상세페이지에서 뒤로가기시 초기화되는 현상 수정

--- src/components/Search.js
+++ src/components/Search.js
@@ -9,7 +9,7 @@ export default class Search extends Component {
   }
   render() {
     this.el.innerHTML = /* html */ `
-      <input placeholder="검색하고 싶은 영화의 제목을 작성해 주세요!"/>
+      <input value="${movieStore.state.searchText}" placeholder="검색하고 싶은 영화의 제목을 작성해 주세요!"/>
       <button>search</button>
     `
 
